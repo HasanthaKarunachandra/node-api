6aa6037681bbe8a37bc40d7fc4368016c49e7826
Hasantha Karunachandra <hkarunachandra@gmail.com>
2024-03-12 17:01:30 +0530

    first commit

---
+++ controller/CustomerController.js
@@ -0,0 +1,109 @@
+const db =require('../database/databaseConnection');
+const bcrypt =require('bcrypt');
+
+const  createCustomer= async (req,resp)=>{
+
+    const hashPassword= await encrypt(req.body.password);
+    const customer={
+        nic:req.body.nic,
+        name:req.body.name,
+        address:req.body.address,
+        password:hashPassword,
+        salary:req.body.salary
+    }
+    const createQuery= 'INSERT INTO customer(nic,name,address,password,salary) VALUES (?,?,?,?,?)';
+    db.query(createQuery,[
+        customer.nic,customer.name,customer.address,customer.password,customer.salary
+    ],(err,result)=>{
+        if(err){
+            console.log(err);
+            return resp.status(500).json({err:'something went wrong'});
+        }
+        return resp.status(201).json({message:'customer was saved!'});
+    });
+
+}
+
+async function encrypt(password){
+    return bcrypt.hash(password,10);
+}
+
+const  findCustomer= async (req,resp)=>{
+    console.log("find working");
+
+    const nic=req.params.nic;
+    const createQuery= 'SELECT * FROM customer WHERE nic=?';
+    db.query(createQuery,[nic],(err,selectedCustomer)=>{
+        if(err){
+            console.log(err);
+            return resp.status(500).json({err:'something went wrong'});
+        }
+    
+        return resp.status(200).json({data:selectedCustomer});
+    });
+    
+    
+}
+
+const  updateCustomer= async(req,resp)=>{
+    console.log("Update working");
+
+    const password=req.params.password;
+
+    const createFetchQuery= 'SELECT * FROM customer WHERE nic=?';
+    db.query(createFetchQuery,[req.body.nic],(err,selectedCustomer)=>{
+        if(err){
+            console.log(err);
+            return resp.status(500).json({err:'something went wrong'});
+        }
+
+
+        bcrypt.compare(password,selectedCustomer[0].password,function(err,result){
+            if(result){
+                const customer={
+                    nic:req.body.nic,
+                    name:req.body.name,
+                    address:req.body.address,
+                    salary:req.body.salary
+                }
+                const updateQuery= 'UPDATE customer SET name=?,address=?,salary=? WHERE nic=?';
+                db.query(updateQuery,[
+                    customer.name,customer.address,customer.salary,customer.password,customer.nic
+                ],(error,updatedRecord)=>{
+                    if(error){
+                        console.log(error);
+                        return resp.status(500).json({error:'something went wrong'});
+                    }
+                    return resp.status(201).json({message:'customer was updated!'});
+                });
+            
+
+
+            }else{
+                return resp.status(403).json({error:'Wrong Password!'});
+            }
+
+        });
+    
+    });
+
+    
+    
+    
+}
+
+const  deleteCustomer= (req,resp)=>{
+    
+}
+
+const  findAllCustomer= (req,resp)=>{
+    
+}
+
+module.exports={
+    createCustomer,  
+    findAllCustomer,
+    updateCustomer,
+    deleteCustomer,
+    findCustomer
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+ const express = require('express');
+ const bodyParser = require('body-parser'); 
+ const app = express();
+
+
+
+ const port=process.env.SERVER_PORT || 3000;
+
+ const customerRoute = require('./route/CustomerRoute');
+
+ app.use(bodyParser.urlencoded({extended:true}));
+ app.use(bodyParser.json());
+
+ 
+ app.listen(port,()=>{
+    console.log(`Server up & running on port ${port}`);
+});
+ 
+ app.use('/api/v1/customers',customerRoute);
